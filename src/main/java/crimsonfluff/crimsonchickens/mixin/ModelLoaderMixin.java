87a3abb9ba15a729fc0501390c53b6bb8382e1c7
waidesoper <waide.soper@gmail.com>
2021-12-01 07:45:59 +1100

    mixin prep

---
+++ src/main/java/crimsonfluff/crimsonchickens/mixin/ModelLoaderMixin.java
@@ -0,0 +1,17 @@
+package crimsonfluff.crimsonchickens.mixin;
+
+import net.minecraft.client.renderer.model.IUnbakedModel;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.ModelLoader;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ModelLoader.class)
+public class ModelLoaderMixin {
+    @Inject(method = "loadModelFromJson", at = @At(value = "INVOKE", target = "Lnet/minecraft/resource/ResourceManager;getResource(Lnet/minecraft/util/Identifier;)Lnet/minecraft/resource/Resource;"), cancellable = true)
+    public void loadModelFromJson(ResourceLocation id, CallbackInfoReturnable<IUnbakedModel> cir) {
+
+    }
+}
